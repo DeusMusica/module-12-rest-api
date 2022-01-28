b541a04e9988fe017e29fc07e6630708a7c9d080
DeusMusica <90479671+DeusMusica@users.noreply.github.com>
2022-01-27 20:09:20 -0600

    added example of seller patch

--- ProductAPI/ProductAPI/Controllers/SellerController.cs
+++ ProductAPI/ProductAPI/Controllers/SellerController.cs
@@ -66,6 +66,32 @@ namespace ProductAPI.Controllers
         //[Route("{Id}")]
         //[ProducesResponseType(StatusCodes.Status201Created)]
         //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //copy over PatchProduct information and add seller info so you can change seller info
+        //public ActionResult<Product> PatchProduct([FromRoute] string productNumber, [FromBody] ProductPatch newProduct)
+        //{
+        //    try
+        //    {
+        //        var productList = _context.Products as IQueryable<Product>;
+        //        var product = productList.First(p => p.ProductNumber.Equals(productNumber));
+
+        //        product.ProductNumber = newProduct.ProductNumber ?? product.ProductNumber;
+        //        product.Department = newProduct.Department ?? product.Department;
+        //        product.Name = newProduct.Name ?? product.Name;
+        //        product.Price = newProduct.Price ?? product.Price;
+        //        product.RelatedProducts = newProduct.RelatedProducts ?? product.RelatedProducts;
+        //        product.LastModified = DateTime.Now;
+
+        //        _context.Products.Update(product);
+        //        _context.SaveChanges();
+
+        //        return new CreatedResult($"/products/{product.ProductNumber.ToLower()}", product);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        // Typically an error log is produced here
+        //        return ValidationProblem(e.Message);
+        //    }
+        //}
 
         [HttpDelete]
         [Route("")]
